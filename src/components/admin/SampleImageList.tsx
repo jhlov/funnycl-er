0b41697a2b5660cbeb1bb8f4db0fd0ffe75c15f9
jhlov <jhlovv@gmail.com>
2023-12-22 23:08:48 +0900

    add sample image

--- src/components/admin/SampleImageList.tsx
+++ src/components/admin/SampleImageList.tsx
@@ -1,63 +1,7 @@
 import { SampleImage } from "interfaces/SampleImage";
 import { useGame } from "store/useGame";
 import "./SampleImageList.scss";
-
-const sampleImageList: Record<string, SampleImage> = {
-  character_1: {
-    type: "character",
-    key: "character_1",
-    url: "/funnycl-er/assets/sample-images/(캐릭터)미래전사.png",
-    name: "미래전사"
-  },
-  character_2: {
-    type: "character",
-    key: "character_2",
-    url: "/funnycl-er/assets/sample-images/(캐릭터)바다위로.png",
-    name: "바다위로"
-  },
-  character_3: {
-    type: "character",
-    key: "character_3",
-    url: "/funnycl-er/assets/sample-images/(캐릭터)사라진보물.png",
-    name: "사라진보물"
-  },
-  character_4: {
-    type: "character",
-    key: "character_4",
-    url: "/funnycl-er/assets/sample-images/(캐릭터)스파이.png",
-    name: "스파이"
-  },
-  character_5: {
-    type: "character",
-    key: "character_5",
-    url: "/funnycl-er/assets/sample-images/(캐릭터)여신.png",
-    name: "여신"
-  },
-  background_1: {
-    type: "background",
-    key: "background_1",
-    url: "/funnycl-er/assets/sample-images/(배경)무인도.png",
-    name: "무인도"
-  },
-  background_2: {
-    type: "background",
-    key: "background_2",
-    url: "/funnycl-er/assets/sample-images/(배경)미래도시.png",
-    name: "미래도시"
-  },
-  background_3: {
-    type: "background",
-    key: "background_3",
-    url: "/funnycl-er/assets/sample-images/(배경)방안(맑음).png",
-    name: "방안"
-  },
-  background_4: {
-    type: "background",
-    key: "background_4",
-    url: "/funnycl-er/assets/sample-images/(배경)여행.jpg",
-    name: "여행"
-  }
-};
+import { sampleImageList } from "./SampleImageListData";
 
 export const SampleImageList = () => {
   const { addNewSampleImage } = useGame();
---
+++ src/components/admin/SampleImageListData.ts
@@ -0,0 +1,282 @@
+import { SampleImage } from "interfaces/SampleImage";
+
+export const sampleImageList: Record<string, SampleImage> = {
+  character_1: {
+    type: "character",
+    key: "character_1",
+    url: "/funnycl-er/assets/sample-images/(캐릭터)미래전사.png",
+    name: "미래전사"
+  },
+  character_2: {
+    type: "character",
+    key: "character_2",
+    url: "/funnycl-er/assets/sample-images/(캐릭터)바다위로.png",
+    name: "바다위로"
+  },
+  character_3: {
+    type: "character",
+    key: "character_3",
+    url: "/funnycl-er/assets/sample-images/(캐릭터)사라진보물.png",
+    name: "사라진보물"
+  },
+  character_4: {
+    type: "character",
+    key: "character_4",
+    url: "/funnycl-er/assets/sample-images/(캐릭터)스파이.png",
+    name: "스파이"
+  },
+  character_5: {
+    type: "character",
+    key: "character_5",
+    url: "/funnycl-er/assets/sample-images/(캐릭터)여신.png",
+    name: "여신"
+  },
+
+  character_6: {
+    type: "character",
+    key: "character_6",
+    url: "/funnycl-er/assets/sample-images/SM01_flame.png",
+    name: "flame"
+  },
+  character_7: {
+    type: "character",
+    key: "character_7",
+    url: "/funnycl-er/assets/sample-images/SM02_wiz.png",
+    name: "wiz"
+  },
+  character_8: {
+    type: "character",
+    key: "character_8",
+    url: "/funnycl-er/assets/sample-images/SM03_vini.png",
+    name: "vini"
+  },
+  character_9: {
+    type: "character",
+    key: "character_9",
+    url: "/funnycl-er/assets/sample-images/SM04_dutti.png",
+    name: "dutti"
+  },
+  character_10: {
+    type: "character",
+    key: "character_10",
+    url: "/funnycl-er/assets/sample-images/SM05_mini.png",
+    name: "mini"
+  },
+  character_11: {
+    type: "character",
+    key: "character_11",
+    url: "/funnycl-er/assets/sample-images/SM06_inou.png",
+    name: "inou"
+  },
+  character_12: {
+    type: "character",
+    key: "character_12",
+    url: "/funnycl-er/assets/sample-images/SM07_intoo.png",
+    name: "intoo"
+  },
+  character_13: {
+    type: "character",
+    key: "character_13",
+    url: "/funnycl-er/assets/sample-images/SM08_clody.png",
+    name: "clody"
+  },
+  character_14: {
+    type: "character",
+    key: "character_14",
+    url: "/funnycl-er/assets/sample-images/SM09_mmuni.png",
+    name: "mmuni"
+  },
+  character_15: {
+    type: "character",
+    key: "character_15",
+    url: "/funnycl-er/assets/sample-images/SM10_inno.png",
+    name: "inno"
+  },
+
+  background_1: {
+    type: "background",
+    key: "background_1",
+    url: "/funnycl-er/assets/sample-images/(배경)무인도.png",
+    name: "무인도"
+  },
+  background_2: {
+    type: "background",
+    key: "background_2",
+    url: "/funnycl-er/assets/sample-images/(배경)미래도시.png",
+    name: "미래도시"
+  },
+  background_3: {
+    type: "background",
+    key: "background_3",
+    url: "/funnycl-er/assets/sample-images/(배경)방안(맑음).png",
+    name: "방안"
+  },
+  background_4: {
+    type: "background",
+    key: "background_4",
+    url: "/funnycl-er/assets/sample-images/(배경)여행.jpg",
+    name: "여행"
+  },
+  background_5: {
+    type: "background",
+    key: "background_5",
+    url: "/funnycl-er/assets/sample-images/BG01_Lobby.png",
+    name: "Lobby 1"
+  },
+  background_6: {
+    type: "background",
+    key: "background_6",
+    url: "/funnycl-er/assets/sample-images/BG01_Lobby2.png",
+    name: "Lobby 2"
+  },
+  background_7: {
+    type: "background",
+    key: "background_7",
+    url: "/funnycl-er/assets/sample-images/BGDot01_Lobby.png",
+    name: "Lobby 3"
+  },
+  background_8: {
+    type: "background",
+    key: "background_8",
+    url: "/funnycl-er/assets/sample-images/BG02_Cafeteria.png",
+    name: "Cafeteria 1-1"
+  },
+  background_9: {
+    type: "background",
+    key: "background_9",
+    url: "/funnycl-er/assets/sample-images/BG02_Cafeteria2.png",
+    name: "Cafeteria 1-2"
+  },
+  background_10: {
+    type: "background",
+    key: "background_10",
+    url: "/funnycl-er/assets/sample-images/BGDot02_Cafeteria.png",
+    name: "Cafeteria 1-3"
+  },
+  background_11: {
+    type: "background",
+    key: "background_11",
+    url: "/funnycl-er/assets/sample-images/BG03_Cafeteria.png",
+    name: "Cafeteria 2-1"
+  },
+  background_12: {
+    type: "background",
+    key: "background_12",
+    url: "/funnycl-er/assets/sample-images/BGDot03_Cafeteria.png",
+    name: "Cafeteria 2-2"
+  },
+  background_13: {
+    type: "background",
+    key: "background_13",
+    url: "/funnycl-er/assets/sample-images/BG03_hallway2.png",
+    name: "hallway 1"
+  },
+  background_14: {
+    type: "background",
+    key: "background_14",
+    url: "/funnycl-er/assets/sample-images/BGDot03_hallway.png",
+    name: "hallway 2"
+  },
+  background_15: {
+    type: "background",
+    key: "background_15",
+    url: "/funnycl-er/assets/sample-images/BG07_hallway2.png",
+    name: "hallway 3"
+  },
+  background_16: {
+    type: "background",
+    key: "background_16",
+    url: "/funnycl-er/assets/sample-images/BGDot05_hallway.png",
+    name: "hallway 1"
+  },
+  background_17: {
+    type: "background",
+    key: "background_17",
+    url: "/funnycl-er/assets/sample-images/BG05_library.png",
+    name: "library 1"
+  },
+  background_18: {
+    type: "background",
+    key: "background_18",
+    url: "/funnycl-er/assets/sample-images/BGDot05_library.png",
+    name: "library 2"
+  },
+  background_19: {
+    type: "background",
+    key: "background_19",
+    url: "/funnycl-er/assets/sample-images/BG06_SecurityGate2.png",
+    name: "SecurityGate 1"
+  },
+  background_20: {
+    type: "background",
+    key: "background_20",
+    url: "/funnycl-er/assets/sample-images/BG06_SecurityGate3.png",
+    name: "SecurityGate 2"
+  },
+  background_21: {
+    type: "background",
+    key: "background_21",
+    url: "/funnycl-er/assets/sample-images/BGDot04_SecurityGate.png",
+    name: "SecurityGate 3"
+  },
+  background_22: {
+    type: "background",
+    key: "background_22",
+    url: "/funnycl-er/assets/sample-images/BG07_lecturehall.png",
+    name: "lecturehall 1"
+  },
+  background_23: {
+    type: "background",
+    key: "background_23",
+    url: "/funnycl-er/assets/sample-images/BGDot07_lecturehall.png",
+    name: "lecturehall 2"
+  },
+  background_24: {
+    type: "background",
+    key: "background_24",
+    url: "/funnycl-er/assets/sample-images/BG08_partition.png",
+    name: "partition 1"
+  },
+  background_25: {
+    type: "background",
+    key: "background_25",
+    url: "/funnycl-er/assets/sample-images/BGDot07_partition3.png",
+    name: "partition 2"
+  },
+  background_26: {
+    type: "background",
+    key: "background_26",
+    url: "/funnycl-er/assets/sample-images/BG09_MeetingRM.png",
+    name: "MeetingRM 1"
+  },
+  background_27: {
+    type: "background",
+    key: "background_27",
+    url: "/funnycl-er/assets/sample-images/BGDot05_MeetingRM.png",
+    name: "MeetingRM 2"
+  },
+  background_28: {
+    type: "background",
+    key: "background_28",
+    url: "/funnycl-er/assets/sample-images/BG10_lounge.png",
+    name: "lounge 1"
+  },
+  background_29: {
+    type: "background",
+    key: "background_29",
+    url: "/funnycl-er/assets/sample-images/BGDot08_lounge.png",
+    name: "lounge 2"
+  },
+  background_30: {
+    type: "background",
+    key: "background_30",
+    url: "/funnycl-er/assets/sample-images/BG12_futurelab.png",
+    name: "futurelab 1"
+  },
+  background_31: {
+    type: "background",
+    key: "background_31",
+    url: "/funnycl-er/assets/sample-images/BGDot12_futurelab.png",
+    name: "futurelab 2"
+  }
+};
