22207319b60e1921d669aa71bbeebe7ce4f62053
jhlov <jhlovv@gmail.com>
2023-12-29 23:52:48 +0900

    [ER-22] 링크 이동

--- src/components/elements/PlaySampleImageElement.tsx
+++ src/components/elements/PlaySampleImageElement.tsx
@@ -1,4 +1,5 @@
-import { PageElement } from "store/useGame";
+import classNames from "classnames";
+import { PageElement, useGame } from "store/useGame";
 
 interface Props {
   multiple: number;
@@ -6,15 +7,32 @@ interface Props {
 }
 
 export const PlaySampleImageElement = (props: Props) => {
+  const { gameInfo, onChangeSelectedPage } = useGame();
+
+  const onClick = () => {
+    if (props.element.link) {
+      const index = gameInfo.pageList.findIndex(
+        page => page.uuid === props.element.link
+      );
+
+      if (index >= 0) {
+        onChangeSelectedPage(index);
+      }
+    }
+  };
+
   return (
     <img
-      className="position-absolute"
+      className={classNames("position-absolute", {
+        "cursor-pointer": props.element.link
+      })}
       style={{
         left: props.element.x * props.multiple,
         top: props.element.y * props.multiple,
         width: props.element.width * props.multiple
       }}
       src={props.element.sampleImage?.url}
+      onClick={onClick}
     />
   );
 };
